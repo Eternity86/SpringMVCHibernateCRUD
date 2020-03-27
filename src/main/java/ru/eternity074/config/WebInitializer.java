ce685c1c9bdede6df87506fa551261b0fe65957b
Infinity86 <arbiter2008@ya.ru>
2020-03-27 19:48:06 +0300

    add encodingFilter

--- src/main/java/ru/eternity074/config/WebInitializer.java
+++ src/main/java/ru/eternity074/config/WebInitializer.java
@@ -1,8 +1,10 @@
 package ru.eternity074.config;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -10,6 +12,11 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		super.onStartup(servletContext);
+		
+		FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encoding-filter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
 	}
 
 	@Override
